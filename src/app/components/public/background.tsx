9d81451aa6046123d1ba11c59d99db5bea613824
minxs33 <theminxs12@gmail.com>
2025-07-17 17:25:05 +0700

    [add] Adding vibrancy effect

--- src/app/components/public/background.tsx
+++ src/app/components/public/background.tsx
@@ -1,7 +1,7 @@
 export default function Background({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <div className="absolute bg-primary inset-0 bg-grid animate-moveGrid z-0" />
+      <div className="absolute inset-0 bg-grid animate-moveGrid z-0" />
       <div className="relative z-10">{children}</div>
     </>
   )
--- src/app/components/public/navigation.tsx
+++ src/app/components/public/navigation.tsx
@@ -21,6 +21,7 @@ export default function Navigation() {
 
   return (
     <>
+    {/* hamburgir */}
       <motion.button 
         whileHover={{ scale: 1.05 }}
         whileTap={{ scale: 0.90 }}
@@ -34,70 +35,77 @@ export default function Navigation() {
           </>
         ) : (
           <>
-            <span className="inline-block w-3/5 h-0.5 bg-gray-800 rounded-full absolute left-1/2 -translate-x-1/2 top-1/2 duration-300 -translate-y-[5px] md:group-hover:rotate-12"></span>
-            <span className="inline-block w-3/5 h-0.5 bg-gray-800 rounded-full absolute left-1/2 -translate-x-1/2 top-1/2 duration-300 translate-y-[5px] md:group-hover:-rotate-12"></span>
+            <span className="inline-block w-3/5 h-0.5 bg-gray-200 rounded-full absolute left-1/2 -translate-x-1/2 top-1/2 duration-300 -translate-y-[5px] md:group-hover:rotate-12"></span>
+            <span className="inline-block w-3/5 h-0.5 bg-gray-200 rounded-full absolute left-1/2 -translate-x-1/2 top-1/2 duration-300 translate-y-[5px] md:group-hover:-rotate-12"></span>
           </>
         )}
-
-        
       </motion.button>
-        <div
-          className={`fixed inset-0 z-10 bg-gray-800 transition-opacity duration-300 ease-in-out ${
-            isOpen ? 'opacity-50 visible' : 'opacity-0 invisible'
-          }`}
-          onClick={() => dispatch(toggleNav())}
-        />
 
-      <div className="fixed inset-y-0 right-0 z-20 w-[80vw] sm:w-[50vw] md:w-[30vw] overflow-hidden">
+      {/* overlay for close nav panel*/}
+      <div
+        className={`fixed inset-0 z-1 transition-opacity duration-300 ease-in-out ${
+          isOpen ? 'opacity-50 visible' : 'opacity-0 invisible'
+        }`}
+        onClick={() => dispatch(toggleNav())}
+      />
+
+      {/* nav panel */}
+      <div className="fixed inset-y-0 right-0 z-1 w-[80vw] sm:w-[50vw] md:w-[30vw] overflow-hidden">
         <motion.div
           initial={{ scaleX: 0 }}
           animate={{ scaleX: isOpen ? 1 : 0 }}
-          transition={{ duration: 0.3, ease: 'easeInOut'}}
+          transition={{ duration: 0.3, ease: 'easeInOut', delayChildren:0}}
           style={{ originX: 1 }}
-          className="absolute inset-0 bg-gray-800 z-1"
-        />
-
-        <AnimatePresence>
-          {isOpen && (
-            <motion.div
-              key="nav"
-              initial={{ scaleX: 0 }}
-              animate={{ scaleX: 1 }}
-              exit={{ scaleX: 0 }}
-              transition={{ duration: 0.3, ease: 'easeInOut', delayChildren: 0.3 }}
-              className="relative h-full w-full origin-right z-1"
-            >
-              <div className="px-16 py-32 space-y-4 text-gray-500">
-                {navItems.map((item, index) => (
-                  <motion.div
-                    key={item.id}
-                    initial={{ opacity: 0, x: 40 }}
-                    animate={{ opacity: 1, x: 0 }}
-                    exit={{ opacity: 0, x: 40 }}
-                    transition={{
-                      duration: 0.3,
-                      delay: index * 0.1,
-                      ease: 'easeInOut',
-                    }}
-                  >
-                    <Link
-                      href={item.href}
-                      className={`block text-lg hover:text-gray-300 ${
-                        activeSection === item.label ? 'text-white' : ''
-                      }`}
-                      onClick={() => {
-                        dispatch(toggleNav());
-                        dispatch(setActiveSection(item.label));
-                      }}
-                    >
-                      {item.label}
-                    </Link>
-                  </motion.div>
-                ))}
-              </div>
-            </motion.div>
-          )}
-        </AnimatePresence>
+          className="absolute inset-0 vibrancy-wrapper z-1 h-full"
+        >
+          <div className="vibrancy-effect" />
+          <div className="vibrancy-tint" />
+          <div className="vibrancy-shine" />
+          <div className="vibrancy-text">
+            {/* Nav Items */}
+            <AnimatePresence>
+              {isOpen && (
+                <motion.div
+                  key="nav"
+                  initial={{ scaleX: 0 }}
+                  animate={{ scaleX: 1 }}
+                  exit={{ scaleX: 0 }}
+                  transition={{ duration: 0.3, ease: 'easeInOut', delayChildren: 0.3 }}
+                  className="relative h-full w-full origin-right z-1"
+                >
+                  <div className="px-16 py-32 space-y-4 text-gray-500">
+                    {navItems.map((item, index) => (
+                      <motion.div
+                        key={item.id}
+                        initial={{ opacity: 0, x: 40 }}
+                        animate={{ opacity: 1, x: 0 }}
+                        exit={{ opacity: 0, x: 40 }}
+                        transition={{
+                          duration: 0.3,
+                          delay: index * 0.1,
+                          ease: 'easeInOut',
+                        }}
+                      >
+                        <Link
+                          href={item.href}
+                          className={`block text-lg hover:text-gray-300 ${
+                            activeSection === item.label ? 'text-white' : ''
+                          }`}
+                          onClick={() => {
+                            dispatch(toggleNav());
+                            dispatch(setActiveSection(item.label));
+                          }}
+                        >
+                          {item.label}
+                        </Link>
+                      </motion.div>
+                    ))}
+                  </div>
+                </motion.div>
+              )}
+            </AnimatePresence>
+          </div>
+        </motion.div>
       </div>
     </>
   )
